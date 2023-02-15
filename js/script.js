df9f28676a4114b2c2849874b6f179f6aebb5f5a
Alexei Penkin <alexei_penkin@mail.ru>
2023-02-15 18:04:37 +0700

    CreateX

--- js/script.js
+++ js/script.js
@@ -1,36 +1,80 @@
-const popupElement = document.querySelector('.popup');
-const closeButton = popupElement.querySelector('.popup__close-button');
-// const profile = document.querySelector('profile');
-const navButton = document.querySelector('.profile__edit-button');
-// let profileName = document.querySelector('.profile__name');
-// let profileJob = document.querySelector('.profile__job');
-const formElement = document.querySelector('.popup__form');
-// let nameInput = formElement.querySelector('[name="name"]');
-// let jobInput = formElement.querySelector('[name="job"]');
+// Переменные - Popup Subscribe //
+const popupSubscribe = document.querySelector(".popup.subscribe");
+const formSubscribe = popupSubscribe.querySelector("form.form-subscribe");
+const navSubscribeButton = document.querySelector(".subscribe__subscribe-btn");
+const popupSubscribeCloseButton = document.querySelector(".popup.subscribe .popup__close-button");
 
-
-function openPopup() {
-    popupElement.classList.add('popup_opened');
-    // nameInput.value = profileName.textContent
-    // jobInput.value = profileJob.textContent
+// Функция открытия модального окна //
+function openPopupSubscribe(popupSubscribe) {
+    popupSubscribe.classList.add("popup_opened");
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    // profileName.textContent = nameInput.value
-    // profileJob.textContent = jobInput.value
-    closePopup();
+// Функция закрытия модального окна //
+function closePopupSubscribe(popupSubscribe) {
+    popupSubscribe.classList.remove("popup_opened");
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-function closePopup() {
-    popupElement.classList.remove('popup_opened');
-}
-navButton.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
-document.addEventListener('keyup', (event) => {
-    if (event.key === "Escape") {
-        closePopup();
-    }
+// Subscribe - кнопка новостной подписки //
+navSubscribeButton.addEventListener("click", () => {
+  openSubscribeForm();
 });
+
+formSubscribe.reset();
+
+// открытие формы Subscribe
+function openSubscribeForm() {
+  openPopupSubscribe(popupSubscribe);
+}
+
+// отправка формы Subscribe
+function submitSubscribeForm(evt) {
+  evt.preventDefault();
+  closePopupSubscribe(popupSubscribe);
+}
+formSubscribe.addEventListener("submit", submitSubscribeForm);
+
+// закрытие формы Subscribe по кнопке закрытия
+popupSubscribeCloseButton.addEventListener("click", () =>
+  closePopupSubscribe(popupSubscribe)
+);
+
+
+// Переменные - Popup Send CV //
+const popupCV = document.querySelector(".popup.cv");
+const formCV = popupCV.querySelector("[name='form-cv']");
+const navCVButton = document.querySelector(".subscribe__submit-btn");
+const popupCVCloseButton = document.querySelector(".popup.cv .popup__close-button");
+
+// Функция открытия модального окна //
+function openPopupCV(popupCV) {
+    popupCV.classList.add("popup_opened");
+  }
+  
+  // Функция закрытия модального окна //
+  function closePopupCV(popupCV) {
+    popupCV.classList.remove("popup_opened");
+  }
+  
+  // Send CV - кнопка отправки резюме //
+  navCVButton.addEventListener("click", () => {
+    openCVForm();
+  });
+  
+  formCV.reset();
+  
+  // открытие формы Subscribe
+  function openCVForm() {
+    openPopupCV(popupCV);
+  }
+  
+  // отправка формы Subscribe
+  function submitCVForm(evt) {
+    evt.preventDefault();
+    closePopupCV(popupCV);
+  }
+  formCV.addEventListener("submit", submitCVForm);
+  
+  // закрытие формы CV по кнопке закрытия
+  popupCVCloseButton.addEventListener("click", () =>
+    closePopupCV(popupCV)
+  );
